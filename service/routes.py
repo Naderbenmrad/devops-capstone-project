d5f9dbbc1a6e6c5b6fa193638ba9779e7e7331a6
naderbenmrad <nader.benmrad@gmail.com>
2025-04-23 17:19:02 -0400

    CRUD Account 2

--- service/routes.py
+++ service/routes.py
@@ -78,28 +78,18 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-<<<<<<< HEAD
     @app.route("/accounts/<int:account_id>", methods=["GET"])
-=======
-@app.route("/accounts/<int:account_id>", methods=["GET"])
->>>>>>> 9020e9d (CRUD Account)
     def get_accounts(account_id):
         """
         Reads an Account
         This endpoint will read an Account based the account_id that is requested
         """
         app.logger.info("Request to read an Account with id: %s", account_id)
-<<<<<<< HEAD
 
         account = Account.find(account_id)
         if not account:
             abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
-=======
-        account = Account.find(account_id)
-        if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
->>>>>>> 9020e9d (CRUD Account)
         return account.serialize(), status.HTTP_200_OK
 
 
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -125,11 +125,7 @@ class TestAccountService(TestCase):
 
     # ADD YOUR TEST CASES HERE ...
 
-<<<<<<< HEAD
     def test_get_account(self):
-=======
- def test_get_account(self):
->>>>>>> 9020e9d (CRUD Account)
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.get(
@@ -138,20 +134,13 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
-<<<<<<< HEAD
     
         def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-=======
-        
-def test_get_account_not_found(self):
-        """It should not Read an Account that is not found"""
-        resp = self.client.get(f"{BASE_URL}/0")
-        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-def test_get_account_list(self):
+    def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
         resp = self.client.get(BASE_URL)
@@ -159,7 +148,7 @@ def test_get_account_list(self):
         data = resp.get_json()
         self.assertEqual(len(data), 5)
 
-def test_update_account(self):
+    def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
         test_account = AccountFactory()
@@ -173,14 +162,13 @@ def test_update_account(self):
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
 
-def test_delete_account(self):
+    def test_delete_account(self):
         """It should Delete an Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
-def test_method_not_allowed(self):
+    def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
->>>>>>> 9020e9d (CRUD Account)
